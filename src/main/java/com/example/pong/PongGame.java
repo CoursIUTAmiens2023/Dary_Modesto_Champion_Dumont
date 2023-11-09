188f808e74407500ce043ed5ad880c7b56fed7e8
JenyoHazuka <tony.dary10@gmail.com>
2023-11-09 08:58:59 +0100

    Création d'un score et ajout d'une rejouabilité

--- src/main/java/com/example/pong/PongGame.java
+++ src/main/java/com/example/pong/PongGame.java
@@ -12,28 +12,40 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
+import javafx.animation.AnimationTimer;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class PongGame extends Application {
-    private static final int WIDTH = 8001;
+    /* Paramètres des différentes tailles */
+    private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
     private static final int PADDLE_HEIGHT = 100;
     private static final int PADDLE_WIDTH = 10;
     private static final int BALL_RADIUS = 10;
 
+    /* Paramètres de la balle */
     private double ballSpeedX = 1;
     private double ballSpeedY = 1;
     private double ballX = WIDTH / 2;
     private double ballY = HEIGHT / 2;
 
+    /* Paramètres des raquettes */
     private double leftPaddleY = HEIGHT / 2 - PADDLE_HEIGHT / 2;
     private double rightPaddleY = HEIGHT / 2 - PADDLE_HEIGHT / 2;
 
+    /* Paramètres d'action des joueurs */
     private boolean isUpKeyPressed1 = false;
     private boolean isDownKeyPressed1 = false;
     private boolean isUpKeyPressed2 = false;
     private boolean isDownKeyPressed2 = false;
+
+    /* Paramètres de score */
+    private int player1Score = 0;
+    private int player2Score = 0;
+    private Label scoreLabel;
 
     @Override
     public void start(Stage stage) {
@@ -68,6 +80,12 @@ public class PongGame extends Application {
             }
         });
 
+        // Création du label de score
+        scoreLabel = new Label("Player 1: 0 - Player 2: 0");
+        scoreLabel.setTextFill(Color.WHITE);
+        scoreLabel.relocate(WIDTH / 2 - 50, 10);
+        ((Pane) scene.getRoot()).getChildren().add(scoreLabel);
+
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), e -> {
             // Mettre à vide l'écran
             gc.setFill(Color.BLACK);
@@ -93,6 +111,27 @@ public class PongGame extends Application {
 
             if (ballX >= WIDTH - PADDLE_WIDTH - BALL_RADIUS && ballY + BALL_RADIUS >= rightPaddleY && ballY <= rightPaddleY + PADDLE_HEIGHT) {
                 ballSpeedX *= -1;
+            }
+
+            // Vérification du marquage d'un point par un joueur
+            if (ballX <= 0) {
+                player2Score++;
+                scoreLabel.setText("Joueur 1: " + player1Score + " - Joueur 2: " + player2Score);
+
+                // Relancer la balle au centre
+                ballX = WIDTH / 2;
+                ballY = HEIGHT / 2;
+                ballSpeedX = -ballSpeedX;
+                ballSpeedY = -ballSpeedY;
+            } else if (ballX >= WIDTH - BALL_RADIUS) {
+                player1Score++;
+                scoreLabel.setText("Joueur 1: " + player1Score + " - Joueur 2: " + player2Score);
+
+                // Relancer la balle au centre
+                ballX = WIDTH / 2;
+                ballY = HEIGHT / 2;
+                ballSpeedX = -ballSpeedX;
+                ballSpeedY = -ballSpeedY;
             }
 
             // Dessiner la balle
